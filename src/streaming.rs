fcdd69aae8164984064f824df54d825e1f381d5e
Bill Jackson <bjackson@earthcomputing.io>
2018-11-16 23:08:50 -0800

    flesh out more details of xmit

--- src/streaming.rs
+++ src/streaming.rs
@@ -2,7 +2,9 @@
 use futures::Future;
 use lazy_static::lazy_static;
 use rdkafka::config::ClientConfig;
-use rdkafka::producer::{FutureProducer, FutureRecord};
+use rdkafka::error::KafkaError;
+use rdkafka::message::OwnedMessage;
+use rdkafka::producer::{ DeliveryFuture, FutureProducer, FutureRecord };
 
 const KAFKA_SERVER: &'static str = "localhost:9092";
 const KAFKA_TOPIC: &'static str = "IPC";
@@ -17,15 +19,16 @@ lazy_static! {
 
 // fn builder<'a>() -> &'a mut ClientConfig { let ref mut factory = ...; factory }
 
+// ref: https://github.com/fede1024/rust-rdkafka/blob/master/examples/asynchronous_processing.rs
 pub fn log(key: String, doclet: String) {
-    let item = FutureRecord::to(KAFKA_TOPIC).payload(&doclet).key(&key);
-    let xmit = PRODUCER_RD.send(item, 0);
+    let item : FutureRecord<String, String> = FutureRecord::to(KAFKA_TOPIC).key(&key).payload(&doclet);
+    let xmit : DeliveryFuture = PRODUCER_RD.send(item, 0);
     // FIXME: lots of things to be handled here
-    let _ = xmit.then(|result| -> Result<(), rdkafka::error::KafkaError> {
+    let _ = xmit.then(|result| -> Result<(), KafkaError> {
         match result {
-            Ok(Ok(_)) => Ok(()),
-            Ok(Err((e, msg))) => Err(e), // Err(e, rdkafka::message::OwnedMessage(msg))
-            Err(e) => Ok(()) // Err(e: futures::Canceled)
+            Ok(Ok(delivery)) => { println!("Sent: {:?}", delivery); Ok(())},
+            Ok(Err((e, msg))) => { println!("Error: {:?}", e); Ok(())}, // Err(e, OwnedMessage(msg))
+            Err(e) => { println!("Cancelled: {:?}", e); Ok(())}         // Err(e: futures::Canceled)
         }
     });
 }
