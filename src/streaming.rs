642a4d133d0362e4ece9c196254ef08ca93aa6ed
Steven Ganz <steven.ganz@teamifier.com>
2021-06-11 20:20:33 -0700

    Add license file and put license/copyright banner on source files.  In the process, ensure that all files end with newline.

--- src/streaming.rs
+++ src/streaming.rs
@@ -1,3 +1,7 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright © 2016-present Earth Computing Corporation. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
 
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::{Arc,Mutex};
